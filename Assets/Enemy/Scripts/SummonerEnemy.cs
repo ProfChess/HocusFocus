8bc6859da7ffc20109a0e03a936952d82f7b094b
ProfChess <122888087+ProfChess@users.noreply.github.com>
2024-06-22 00:40:50 -0300

    Summoner Enemy summoning and running added
    summoner can summon flying enemies. Can also run away if player gets too close

--- Assets/Enemy/Scripts/SummonerEnemy.cs
+++ Assets/Enemy/Scripts/SummonerEnemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor.XR;
 using UnityEngine;
 
 public class SummonerEnemy : BaseEnemyMovement
@@ -11,10 +12,18 @@ public class SummonerEnemy : BaseEnemyMovement
 
     //Running Variables
     private bool run = false;
+    private int runningFreq = 4;
+    private float runCDTimer = 0f;
+    private bool canRun = false;
+    private Vector2 dashTarget;
+
+    //Summoning
+    public GameObject flyingEnemyPrefab;
+    private bool summoning = false;
+    private bool playerRight = false;
 
     //Wondering Variables
-    private int wonderFreq = 5;
-    private Vector2 wonderDirection;
+    private int wonderFreq = 3;
     public float wonderMinDistance = 2;
     public float wonderMaxDistance = 4;
     private Vector2 targetToWalk;
@@ -23,7 +32,7 @@ public class SummonerEnemy : BaseEnemyMovement
 
     protected void Awake()
     {
-        Initialize(2, 4, 0, false, 100, 2);
+        Initialize(2, 4, 0, false, 10, 4);
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -34,20 +43,40 @@ public class SummonerEnemy : BaseEnemyMovement
     protected void Update()
     {
         //Player gets too close -> summoner will run away
-        run = player.transform.position.x <= transform.position.x + EnemyAttackRange &&
-             player.transform.position.x >= transform.position.x - EnemyAttackRange;
+        run = Vector3.Distance(transform.position, player.transform.position) <= EnemyAttackRange;
+        //Player Direction
+        playerRight = transform.position.x <= player.transform.position.x;
 
-        if (canWonder)
+        if (canWonder && !canSeePlayer)
         {
             enemyWonderState();
         }
-        else
+        else if (!canSeePlayer) 
         {
             idleTimer -= Time.deltaTime;
             if(idleTimer < 0f)
             {
                 calculateTargetPosition();
                 canWonder = true;
+            }
+        }
+
+        else if (canSeePlayer && !summoning && !run)
+        {
+            enemySummonState();
+        }
+
+        else if (canSeePlayer && !summoning && run && canRun)
+        {
+            enemyRunAwayState();
+        }
+        else if (!canRun)
+        {
+            runCDTimer -= Time.deltaTime;
+            if(runCDTimer < 0f)
+            {
+                findDashTarget();
+                canRun = true;
             }
         }
     }
@@ -69,26 +98,69 @@ public class SummonerEnemy : BaseEnemyMovement
         float wonderDistance = Random.Range(wonderMinDistance, wonderMaxDistance);
         if (wonderDir == 1)
         {
-            wonderDirection = Vector2.right;
             targetToWalk = transform.position;
             targetToWalk.x += wonderDistance;
         }
         else if (wonderDir == 2)
         {
-            wonderDirection = Vector2.left;
             targetToWalk = transform.position;
             targetToWalk.x -= wonderDistance;
         }
     }
 
+    protected void findDashTarget()
+    {
+        //dash target
+        float distanceToRight = Vector2.Distance(transform.position, rightLeash.position);
+        float distanceToLeft = Vector2.Distance(transform.position, leftLeash.position);
+        
+        if (distanceToRight >= distanceToLeft)
+        {
+            dashTarget = rightLeash.position;
+        }
+        else if (distanceToLeft >= distanceToRight)
+        {
+            dashTarget = leftLeash.position;
+        }
+
+    }
     protected void enemyRunAwayState()
     {
+        transform.position = Vector2.MoveTowards(transform.position, dashTarget, EnemyChaseSpeed * Time.deltaTime);
 
+        if (Vector2.Distance(transform.position, leftLeash.position) <= 0.1f ||
+            Vector2.Distance(transform.position, rightLeash.position) <= 0.1f)
+        {
+            canRun = false;
+            runCDTimer = runningFreq;
+            findDashTarget();
+        }
     }
 
     protected void enemySummonState()
     {
+        if (Vector2.Distance(transform.position, player.transform.position) <= EnemyDetectRange &&
+            Vector2.Distance(transform.position, player.transform.position) > EnemyAttackRange)
+        {
+            StartCoroutine(summonFlyingEnemy());
+        }
+    }
 
+    private IEnumerator summonFlyingEnemy()
+    {
+        summoning = true;
+        //Animation trigger
+
+        yield return new WaitForSeconds(0.5f); //Length of summoning anim
+
+        //Summoning Logic
+        Vector3 summonPosition = transform.position;
+        summonPosition.x += 1;
+        summonPosition.y += 1;
+        Instantiate(flyingEnemyPrefab, summonPosition, Quaternion.identity);
+
+        yield return new WaitForSeconds(1f); //Summoning cooldown
+        summoning = false;
     }
 
 }
