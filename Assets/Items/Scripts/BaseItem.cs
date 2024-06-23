42ac5a0a1691c1dad0bc373fed3135d28dfd5429
ProfChess <122888087+ProfChess@users.noreply.github.com>
2024-06-23 00:10:06 -0300

    Items Added
    Added dash cloak, teleport talisman, and wingboots that give player movement abilities

---
+++ Assets/Items/Scripts/BaseItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class BaseItem : MonoBehaviour
+{
+
+    private BoxCollider2D hitBox;
+
+    protected virtual void Start()
+    {
+        hitBox = GetComponent<BoxCollider2D>();
+    }
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && hitBox != null)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                gameObject.SetActive(false);
+                Invoke("destroyItem", 1);
+            }
+        }
+    }
+
+    protected virtual void destroyItem()
+    {
+        Destroy(gameObject);
+    }
+}
